b41cce101a4ea7323236027bdde8ef27e60005d8
rainbowsec <59576967+rainbowsec@users.noreply.github.com>
2020-01-10 16:04:53 +0100

    largest_prime_factor

---
+++ largest_prime_factor/src/main.rs
@@ -0,0 +1,34 @@
+fn is_prime(n: u64) -> bool {
+    let mut i = 3;
+    let square_root_of_n = (n as f64).sqrt() as u64 + 1;
+    if n <= 3 && n >= 1 {
+        return true
+    }
+    while i < square_root_of_n {
+        if n % i == 0 {
+            return false
+        }
+        i += 2;
+    }
+    true
+}
+
+fn largest_prime_factor(n: u64) -> u64 {
+    let mut prime_factor = n;
+    let square_root_of_n = (n as f64).sqrt() as u64 + 1;
+    for i in 2..square_root_of_n {
+        if is_prime(i) {
+            if n % i == 0 {
+                prime_factor = i;
+            }
+        }
+    }
+    prime_factor
+}
+
+fn main() {
+    let nums = vec![2, 3, 5, 7, 13195, 600851475143];
+    for num in nums {
+        println!("the largest prime factor for {} is {}", num, largest_prime_factor(num));
+    }
+}
