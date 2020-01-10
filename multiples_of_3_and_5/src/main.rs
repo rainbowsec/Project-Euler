72df390c987795bc27aaad4236702b690033c3fb
rainbowsec <59576967+rainbowsec@users.noreply.github.com>
2020-01-10 17:06:16 +0100

    Added formal test to multiples_of_3_and_5

--- multiples_of_3_and_5/src/main.rs
+++ multiples_of_3_and_5/src/main.rs
@@ -8,9 +8,22 @@ fn multiples_of_3_and_5(number: u32) -> u32 {
     sum
 }
 
-fn main() {
-    let nums = vec![10, 49, 1000, 19564, 8456];
-    for num in nums {
-        println!("The sum of all integers divisible by 3 or 5 under {} is {}", num, multiples_of_3_and_5(num));
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiples_of_3_and_5() {
+        let nums = vec![10, 49, 1000, 19564, 8456];
+        let expected = vec![23, 543, 233168, 89301183, 16687353];
+        let i = nums.len() - 1;
+        for j in 0..i {
+            assert_eq!(multiples_of_3_and_5(nums[j]), expected[j]);
+        }
+
     }
+}
+
+fn main() {
+    println!("To run the test suite: cargo test");
 }
